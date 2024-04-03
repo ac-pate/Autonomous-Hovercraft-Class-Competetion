b13d635ee64ddf2d88b49a129d3aa9c78f59050d
liyan19 <98620816+liyan19@users.noreply.github.com>
2024-04-03 14:55:49 -0400

    Added IMU and US sensor readings
    Added basic functions to read values for US sensor and IMU

--- ENGR290/src/main.cpp
+++ ENGR290/src/main.cpp
@@ -1,18 +1,72 @@
+/*=================================================================================================
+                                                Includes
+=================================================================================================*/
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include "Servo.h"
+#include <Wire.h>
+#include "MPU6050_light.h"
+/*=================================================================================================
+                                                Defines
+=================================================================================================*/
+#define TRIG_PIN 13
+#define ECHO_PIN 3
+#define SERVO_PIN 6
+#define FAN_PIN 4
+/*=================================================================================================
+                                                Variables
+=================================================================================================*/
+Servo servo;
+int distance;
+long duration;
+MPU6050 mpu(Wire);
+ 
+ /*=================================================================================================
+                                                Prototypes
+=================================================================================================*/
+/*=================================================================================================
+                                                Setup
+=================================================================================================*/
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  Wire.begin();
+  byte status = mpu.begin();
+  while(status!=0){ }//stop if IMU is not found
+  servo.attach(SERVO_PIN);
+  delay(1000);
+  mpu.calcOffsets(true,true);
+  Wire.setWireTimeout(3000, true);
 
+}
+/*=================================================================================================
+                                                Loop
+=================================================================================================*/
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
+  mpu.update();
+  Serial.print("Angle X: ");
+  Serial.println(mpu.getAngleX());
+  Serial.print("Angle Y: ");
+  Serial.println(mpu.getAngleY());
+  Serial.print("Angle Z: ");
+  Serial.println(mpu.getAngleZ());
+  Serial.println(" ");
+  delay(100);
 }
+/*=================================================================================================
+                                                Interrupts
+=================================================================================================*/
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+/*=================================================================================================
+                                                Functions
+=================================================================================================*/
